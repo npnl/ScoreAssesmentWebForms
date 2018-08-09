294b9159265f8a040c50a254bd585bd64bacb2e0
Amit Kumar <amitasviper@gmail.com>
2018-08-09 12:21:07 -0700

    Add two pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
+import Home from './Home';
+import Post from './Post';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<div className="App">
+					<header className="App-header">
+						<Link to="/">
+							<img src={logo} className="App-logo" alt="logo" />
+						</Link>
+						<h1 className="App-title">Welcome to React</h1>
+						<Link to="/">Home</Link>
+						<Link to="/post/">Post</Link>
+
+					</header>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/post/" component={Post} />
+					</Switch>
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
