f04bbb3fd6b6a34c4fde250caa4cd459eb267548
Amit Kumar <amitasviper@gmail.com>
2019-03-08 11:47:57 -0800

    Update the backend address

--- src/_constants/server.constants.js
+++ src/_constants/server.constants.js
@@ -1,6 +1,6 @@
 export const serverConstants = {
   // BASE_URL: 'http://54.153.47.191:80', // Integration West Zone
-  BASE_URL: 'http://35.153.226.68:80', // Production: East zone
+  BASE_URL: 'http://18.209.109.171:80', // Production: East zone
   // BASE_URL: 'http://localhost:5000',
 
   UI_RELATIVE_PATH: '',
